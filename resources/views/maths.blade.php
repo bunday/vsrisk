6a846b05e64bca108838a4ef7fe40b2409bd3f16
Olabode <seyyids90@gmaail.com>
2016-10-24 13:51:41 +0100

    latest

---
+++ resources/views/maths.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Acceptance Criteria</div>
+
+                <div class="panel-body">
+                    <div>
+                        <--Header-->
+                        <div class="row">
+                            <div class="col-md-3">
+                            <h4>Select a Formular</h4></div>
+                            <div class="col-md-6">
+                            <select class="form-control" id="fml" onchange="">
+                              <option>[likelihood + impact] - 1</option>
+                              <option>[likelihood + impact]</option>
+                              <option>[likelihood * impact] - 1</option>
+                              <option>[likelihood * impact]</option>
+                            </select></div>
+                            <input type="hidden" name="clike" id="clike" value="7">
+                            <input type="hidden" name="cimp" id="cimp" value="7">
+                            <div class="col-md-3">
+                                <button class="btn btn-primary" onclick="clearit(); solve();  gitt();dynamicmatrix();">Calculate</button>
+                            </div>
+                        </div>
+                        <br/><br/>
+                        <div class="row">
+                            <div class="col-md-3">Likelihood</div>
+                            <div class="col-md-9">Graph Here
+                                <br/><br/>
+                                <div class="row"> 
+                                <div class="col-md-1">
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">7</div><br/>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">6</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">5</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">4</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">3</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">2</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">1</div>
+                                    <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">X</div>
+                                </div>
+                            <div class="col-md-9" id="holder">
+                            <div class="row">
+                                <div class="col-md-1" id="a1"></div>
+                                <div class="col-md-1" id="a2"></div>
+                                <div class="col-md-1" id="a3"></div>
+                                <div class="col-md-1" id="a4"></div>
+                                <div class="col-md-1" id="a5"></div>
+                                <div class="col-md-1" id="a6"></div>
+                                <div class="col-md-1" id="a7"></div>
+                                </div>
+                                <div>
+                                <div class="col-md-1 btn btn-primary" style="width: 55px; height: 50px;">1</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 55px; height: 50px;">2</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">3</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 50px; height: 50px;">4</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 55px; height: 50px;">5</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 55px; height: 50px;">6</div>
+                                <div class="col-md-1 btn btn-primary" style="width: 52px; height: 50px;">7</div>
+
+                            </div>
+                            </div>
+
+                            
+                            </div>
+                            </div>
+                            <div class="col-md-3">1</div>
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
